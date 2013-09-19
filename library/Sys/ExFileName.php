b684b5f168cf88112e21bc640bf9090efb2a00a9
londoaaa <andrianbahaodien@gmail.com>
2013-09-20 01:44:18 +0700

    tambahan

---
+++ library/Sys/ExFileName.php
@@ -0,0 +1,33 @@
+<?php
+
+class Sys_ExFileName {
+// 	JQUERY
+	const JQUERY_JS				= 'plugins/jquery/js/jquery-1.9.1.js';
+	const JQUER_UI_JS			= 'plugins/jquery/js/jquery-ui-1.10.3.custom.min.js';
+	const JQUERY_ORANGE_CSS		= 'plugins/jquery/css/orange-theme/jquery-ui-1.10.3.custom.min.css';
+	
+// 	TWITTER BOOTSTRAP
+	const BT3S_JS				= 'plugins/twitter-bootstrap/3.0.0.S/js/bootstrap.min.js';//Ori
+	const BT3S_CSS				= 'plugins/twitter-bootstrap/3.0.0.S/css/bootstrap.min.css';//Ori Bootstrap 3.0.0
+	
+	const BT3_JS				= 'plugins/twitter-bootstrap/3.0.0/js/bootstrap.min.js';//Ori
+	const BT3_CSS				= 'plugins/twitter-bootstrap/3.0.0/css/bootstrap.min.css';//Ori Bootstrap 3.0.0
+	
+	const BT_JS					= 'plugins/twitter-bootstrap/2.3.2/js/bootstrap.min.js';//Ori
+	const BT_CSS				= 'plugins/twitter-bootstrap/2.3.2/css/bootstrap.min.css';//Ori Bootstrap 2.3.2
+	
+	const BT3_1_CSS				= 'plugins/twitter-bootstrap/3.0.0/css/bootstrap1.css';//Compiled Bootstrap 3.0.0 #1
+	const BT3_2_CSS				= 'plugins/twitter-bootstrap/3.0.0/css/bootstrap2.css';//Compiled Bootstrap 3.0.0 #2
+	const BT_1_CSS				= 'plugins/twitter-bootstrap/2.3.2/css/bootstrap1.css';//Compiled Bootstrap 2.3.2 #1
+	
+// 	ICOMOON
+	const ICOMOON1_CSS			= 'plugins/icomoon/style.css';
+	
+// 	LESS
+	const LESS_JS 				= 'less/less1.4.1.min.js';
+	const BT_HOME_LESS			= 'less/bt-home.less';
+	const BT_ETIS_LESS			= 'less/bt-etis.less';
+	
+// 	CSS
+	const STICKY_FOOTER_CSS		= 'css/sticky-footer.css';
+}
